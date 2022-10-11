43a5a43f8bf403a8e7279c37db3646a5da206960
Severo-Yannick <severoyannick@gmail.com>
2022-10-11 08:38:52 +0200

    generate a secret number

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 // The std::io module contains a number of common things you'll need when doing input and output.
 use std::io;
+// Rand provides utilities to generate random numbers, to convert them to useful types and distributions, and some randomness-related algorithms.
+use rand::Rng;
 
 fn main() {
     println!("Devinez le nombre !");
     println!("Veuillez entrer un nombre !");
+    let range = 1..101;
+    let secret_number = rand::thread_rng().gen_range(range);
+    println!("Le nombre secret aleatoire est: {secret_number}");
     // Create a new empty String ""
-    let mut supposition = String::new();
+    let mut assumption = String::new();
     // Constructs a new handle to the standard input of the current process.
     io::stdin()
-        .read_line(&mut supposition)
+        .read_line(&mut assumption)
         .expect("Echec de la lecture de l'entrée utilisateur");
 
-        println!("Votre nombre : {supposition}");
+        println!("Votre nombre : {assumption}");
 }
