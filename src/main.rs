e3edded56e4f74d474784736cb5aa29a220ae821
Severo-Yannick <severoyannick@gmail.com>
2022-10-15 11:17:26 +0200

    user score

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,40 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Devinez le nombre !");
-    println!("Veuillez entrer un nombre !");
     let range = 1..101;
     let secret_number = rand::thread_rng().gen_range(range);
-    println!("Le nombre secret aleatoire est: {secret_number}");
-    // Create a new empty String ""
-    let mut assumption = String::new();
-    // Constructs a new handle to the standard input of the current process.
-    io::stdin()
-        .read_line(&mut assumption)
-        .expect("Echec de la lecture de l'entrée utilisateur");
+    let mut _score: u8 = 0;
+    let _win: &str = "\u{2705} Vous avez gagné !";
+    let _minus: &str = "\u{2796} C'est moins !";
+    let _plus: &str = "\u{2795} C'est plus !";
+    let _result: &str = "\u{1F449} Votre scrore :";
+    // println!("Le nombre secret aleatoire est: {secret_number}");
     
-        let assumption: u32 = assumption.trim().parse().expect("Veuillez entrer un nombre !");
-
-        println!("Votre nombre : {assumption}");
-        // match control flow based on pattern matching.
-        match assumption.cmp(&secret_number) {
-            Ordering::Less => println!("C'est plus !"),
-            Ordering::Greater => println!("C'est moins !"),
-            Ordering::Equal => println!("Vous avez gagné !"),
-        }
+    loop {
+        _score += 1;
+        println!("Veuillez entrer un nombre !");
+        // Create a new empty String ""
+        let mut assumption = String::new();
+        // Constructs a new handle to the standard input of the current process.
+        io::stdin()
+            .read_line(&mut assumption)
+            .expect("Echec de la lecture de l'entrée utilisateur");
+        
+            let assumption: u32 = match assumption.trim().parse() {
+                Ok(number) => number,
+                Err(_) => continue,
+            };
+    
+            println!("Votre nombre : {assumption}");
+            // match control flow based on pattern matching.
+            match assumption.cmp(&secret_number) {
+                Ordering::Less => println!("{}", _plus),
+                Ordering::Greater => println!("{}", _minus),
+                Ordering::Equal => {
+                    println!("{}", _win);
+                    println!("{_result} {_score}");
+                    break;
+                }
+            }
+    }
 }
